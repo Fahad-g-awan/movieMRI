0bccf0e42c57eab00d99e773e8c20ce41ccda7bf
Fahad <fahadghaffar.awan@gmail.com>
2023-02-10 22:38:19 +0500

    f

--- frontend/src/api/clinet.js
+++ frontend/src/api/clinet.js
@@ -5,6 +5,7 @@ import axios from "axios";
 
 const client = axios.create({
   baseURL: "https://moviemri-backend.onrender.com/api",
+  // baseURL: "http://localhost:8000/api",
 });
 
 export default client;
