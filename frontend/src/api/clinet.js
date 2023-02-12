7862de0cc6af43f7f62700b1c98f0b8c6d1f7b2b
Fahad <fahadghaffar.awan@gmail.com>
2023-02-12 17:51:12 +0500

    changed baseurl

--- frontend/src/api/clinet.js
+++ frontend/src/api/clinet.js
@@ -1,11 +1,8 @@
 import axios from "axios";
 
-// "https://moviemri-backend.onrender.com/api"
-// "http://localhost:8000/api";
-
 const client = axios.create({
-  // baseURL: "https://moviemri-backend.onrender.com/api",
-  baseURL: "http://localhost:8000/api",
+  baseURL: "https://moviemri-backend.onrender.com/api",
+  // baseURL: "http://localhost:8000/api",
 });
 
 export default client;
