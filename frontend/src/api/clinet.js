222b92c85ef3755dc2f1531d6444747cc8decf81
Fahad G. Awan <106470048+Fahad-g-awan@users.noreply.github.com>
2023-02-13 18:16:08 +0500

    Update clinet.js

--- frontend/src/api/clinet.js
+++ frontend/src/api/clinet.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const client = axios.create({
   // baseURL: "https://moviemri-backend.onrender.com/api",
-  baseURL: "http://localhost:8000/api",
+//   baseURL: "http://localhost:8000/api",
+  baseURL: "https://moviemri-production.up.railway.app/api",
 });
 
 export default client;
